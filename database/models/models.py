6972498f11d910eccdb9a2863bb8f44734008cb2
Artikbee <105461731+Artikbee@users.noreply.github.com>
2024-06-04 01:29:21 +0200

    Update models.py

--- database/models/models.py
+++ database/models/models.py
@@ -1,18 +1,29 @@
 from enum import Enum
-from sqlalchemy import Boolean
+from sqlalchemy import func, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from datetime import datetime
-from typing import Optional
+from typing import Annotated
 import pytz
 
 timezone = pytz.timezone('Europe/Belgrade')
+intpk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime, mapped_column(server_default=func.now(timezone), nullable=False)]
+update_at = Annotated[datetime, mapped_column(
+    server_default=func.now(timezone),
+    nullable=False,
+    onupdate=func.now(timezone),
+)]
+
+str_256 = Annotated[str, '256']
 
 
 class Base(DeclarativeBase):
-    pass
+    type_annotation_map = {
+        str_256: String(256),
+    }
 
 
-class Role(str, Enum):
+class Role(Enum):
     ROLE_USER = 'ROLE_USER'
     ROLE_ADMIN = 'ROLE_ADMIN'
 
@@ -20,14 +31,32 @@ class Role(str, Enum):
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    telegram_id: Mapped[int] = mapped_column()
-    chat_id: Mapped[int] = mapped_column()
+    id: Mapped[intpk]
+    telegram_id: Mapped[int]
+    chat_id: Mapped[int]
     role: Mapped[Role] = mapped_column(default=Role.ROLE_USER)
-    is_blocked: Mapped[Boolean] = mapped_column(Boolean, default=False)
-    created_at: Mapped[Optional[datetime]] = mapped_column(default=datetime.now(tz=timezone))
-    modified_at: Mapped[Optional[datetime]] = mapped_column()
+    is_blocked: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[created_at]
+    update_at: Mapped[update_at]
 
     @property
     def is_admin(self) -> bool:
         return Role.ROLE_ADMIN in self.role
+
+
+class Category(Base):
+    __tablename__ = "categories"
+    id: Mapped[intpk]
+    title: Mapped[str_256]
+    created_at: Mapped[created_at]
+    update_at: Mapped[update_at]
+
+
+class Product(Base):
+    __tablename__ = "products"
+    id: Mapped[intpk]
+    title: Mapped[str_256]
+    description: Mapped[str_256]
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id", ondelete="CASCADE"))
+    created_at: Mapped[created_at]
+    update_at: Mapped[update_at]
